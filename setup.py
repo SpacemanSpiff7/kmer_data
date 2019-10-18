206facfbd4215888f184e0883f7c0e02fab8107b
Simone Longo <simone.longo@gmail.com>
2019-10-18 15:02:28 -0600

    add setup.py and LICENSE.txt

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='TowelStuff',
+    version='0.1.0',
+    author='Simone Longo',
+    author_email='s.longo@utah.edu',
+    packages=['kmertools'],
+    license='LICENSE.txt',
+    description='Tools for processing VCF files in parallel in addition to k-mer search and analysis',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "pyfaidx",
+        "cyvcf2",
+        "pandas",
+        "numpy",
+    ],
+)
